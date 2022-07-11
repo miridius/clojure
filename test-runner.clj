dc8268eb1063b5d657c27346ea82a4aedf2a03a1
David Rolle <miridius@miridi.us>
2022-07-11 12:12:06 +0200

    WIP - update test-runner & test script

--- test-runner.clj
+++ test-runner.clj
@@ -6,10 +6,10 @@
          '[clojure.string :as str]
          '[rewrite-clj.zip :as z])
 
-;(cp/add-classpath "libs.jar")
+(cp/add-classpath "libs.jar")
 
-#_(require '[clojure.spec.alpha :as s]
-           '[clojure.spec.gen.alpha :as gen])
+(require '[clojure.spec.alpha :as s]
+         '[clojure.spec.gen.alpha :as gen])
 
 ;; Add solution source and tests to classpath
 (def slug (first *command-line-args*))
@@ -29,61 +29,101 @@
   [loc]
   (= (symbol 'deftest) (-> loc z/down z/sexpr)))
 
+(defn assertion?
+  "Returns true if the given node is an `is`."
+  [loc]
+  (= (symbol 'is) (-> loc z/down z/sexpr)))
+
 (defn test-name
   "Returns the name of the test at a given node."
   [loc]
   (-> loc z/down z/right z/sexpr))
 
 (defn test-code
-  "Returns the code of the test at a given node."
+  "Returns all of the test code for a given test"
   [loc]
-  (-> loc z/down z/right z/right z/sexpr))
+  (->> (z/child-sexprs loc)
+       (drop 2)
+       (str/join "\n")))
 
-(defn tests
-  "Traverses a zipper representing a parsed test file.
-   Returns a vector of the test names in the order defined."
-  [z]
-  (loop [loc z tests []]
-    (cond
-      (nil? loc) tests
-      (test? loc) (recur (z/right loc) (conj tests (test-name loc)))
-      :else (recur (z/right loc) tests))))
+(defn find-assertions
+  "Returns a vector of the code for individual assertions in a given test."
+  [loc]
+  (->> (z/of-string (z/->string loc)) ;; isolate subtree to prevent find from leaving (is there a better way?)
+       (iterate #(z/find-next-depth-first % assertion?))
+       rest
+       (take-while some?)
+       (map #(->> (iterate z/up %)
+                  (take-while (complement test?))
+                  last))
+       (map z/sexpr)
+       (mapv str)))
 
-(defn test-codes
-  "Traverses a zipper representing a parsed test file.
-   Returns a vector of the test codes in the order defined."
-  [z]
-  (loop [loc z tests []]
-    (cond
-      (nil? loc) tests
-      (test? loc) (recur (z/right loc) (conj tests (test-code loc)))
-      :else (recur (z/right loc) tests))))
+(defn for-each-test
+  "Traverses the zipper representing the parsed test file.
+   Returns the result of calling f on each test."
+  [f]
+  (->> (iterate z/right zloc)
+       (take-while some?)
+       (filter test?)
+       (map f)))
 
-(defn test-code-map [loc]
-  (zipmap (tests loc) (test-codes loc)))
+(def tests
+  "A vector of the test names in the order defined."
+  (for-each-test test-name))
+
+(def test-codes
+  "A vector of the test codes in the order defined."
+  (for-each-test test-code))
+
+(def test-assertions
+  "A vector of vectors of test assertions in the order defined."
+  (for-each-test find-assertions))
+
+(def test-code-map (zipmap tests test-codes))
+(def test-assertions-map (zipmap tests test-assertions))
 
 ;; State to hold test results
 (def passes (atom []))
-(def fails (atom []))
-(def errors (atom []))
+(def fails+errors (atom []))
+(def assertion-counts (atom {})) ;; tracks which assertion is being reported on
+
+;; logic for creating test results
+
+(defn get-message [m status]
+  (case status
+    "fail" (str "Expected " (:expected m) " but got " (:actual m))
+    "error" (str "An unexpected error occurred:\n" (:actual m))
+    "pass" nil))
+
+(defn get-test-code [test-name status]
+  (let [a-count ((swap! assertion-counts update test-name (fnil inc 0)) test-name)]
+    (if (= "pass" status)
+      ;; for passing tests show the whole test
+      (test-code-map test-name)
+      ;; for fails just the one that failed
+      ((test-assertions-map test-name) (dec a-count)))))
+
+(defn report [m status]
+  (let [name (:name (meta (first t/*testing-vars*)))
+        message (get-message m status)]
+    (merge {:name name :status status :test_code (get-test-code name status)}
+           (when message {:message message}))))
 
 ;; Override clojure.test reporting methods to capture their results
 
-(defmethod t/report :begin-test-ns [m])
+(defmethod t/report :begin-test-ns [_])
 
 (defmethod t/report :pass [m]
-  (swap! passes conj {:name (:name (meta (first t/*testing-vars*)))
-                      :status "pass"}))
+  (swap! passes conj (report m "pass")))
 
 (defmethod t/report :fail [m]
-  (swap! fails conj {:name (:name (meta (first t/*testing-vars*)))
-                     :status "fail"
-                     :message (str "Expected " (:expected m) " but got " (:actual m))}))
+  (swap! fails+errors conj (report m "fail")))
 
 (defmethod t/report :error [m]
-  (swap! errors conj (:name (meta (first t/*testing-vars*)))))
+  (swap! fails+errors conj (report m "error")))
 
-(defmethod t/report :summary [m])
+(defmethod t/report :summary [_])
 
 (t/run-tests test-ns)
 
@@ -91,19 +131,10 @@
 
 (println (json/generate-string
           {:version 2
-           :status (if (and (empty? @fails)
-                            (empty? @errors))
-                     "pass" "fail")
-           :tests (vec (for [test (tests zloc)]
-                         (cond
-                           (contains? (set (map :name @passes)) test)
-                           {:name test :status "pass" :test_code (str (test (test-code-map zloc)))}
-                           (contains? (set (map :name @fails)) test)
-                           {:name test :status "fail" :test_code (str (test (test-code-map zloc)))
-                            :message (:message (first (filter #(= test (:name %)) @fails)))}
-                           (contains? (set (set @errors)) test)
-                           {:name test :status "error" :test_code (str (test (test-code-map zloc)))
-                            :message (:message (first (filter #(= test (:name %)) @errors)))})))}
-          {:pretty true}))
+           :status (if (empty? @fails+errors) "pass" "fail")
+           :tests (for [test tests]
+                    (->> (concat @fails+errors @passes) ;; failure takes priority!
+                         (filter #(= test (:name %)))
+                         first))}))
 
 (System/exit 0)
--- test.clj
+++ test.clj
@@ -35,21 +35,25 @@
                   (str root "exercises/concept/" slug "/.meta/exemplar.clj"))
         src (if practice?
               (str root "exercises/practice/" slug "/src/" (->snake_case slug) ".clj")
-              (str root "exercises/concept/" slug "/src/" (->snake_case slug) ".clj"))]
-    (shell/sh "cp" example src)
-    (= "pass" ((json/parse-string
-                (:out (shell/sh (str test-runner-dir "test-runner.clj")
-                                slug
-                                (str root (if practice? "exercises/practice/" "exercises/concept/") slug "/")
-                                (str root (if practice? "exercises/practice/" "exercises/concept/") slug "/"))))
-               "status"))))
+              (str root "exercises/concept/" slug "/src/" (->snake_case slug) ".clj"))
+        _ (shell/sh "cp" example src)
+        result (shell/sh (str test-runner-dir "test-runner.clj")
+                         slug
+                         (str root (if practice? "exercises/practice/" "exercises/concept/") slug "/")
+                         (str root (if practice? "exercises/practice/" "exercises/concept/") slug "/"))
+        output (-> result :out json/parse-string)]
+    (if (= "pass" (get output "status"))
+      true
+      (do (println slug "failed:" (json/generate-string output {:pretty true}))
+          (when (:err result) (println (:err result)))
+          false))))
 
 (defn test-exercises! []
   (for [exercise (into practice-exercises concept-exercises)]
-    {(keyword exercise) (test-exercise exercise)}))
+    [exercise (test-exercise exercise)]))
 
 (let [results (test-exercises!)
-      fails (filter #(false? (first (vals %))) results)]
+      fails (->> results (remove second) (mapv first))]
   (prn {:tested (count results)
         :fails fails})
   (System/exit (count fails)))
